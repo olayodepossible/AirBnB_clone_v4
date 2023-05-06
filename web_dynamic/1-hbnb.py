9beeb38a876ac075231bdd65416be94f098e1f6f
Akinfemiwa <possibleolayode5344@gmail.com>
2023-05-06 08:03:51 +0000

    implement show and hide reviews

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -7,7 +7,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-import uuid
+from uuid import uuid4
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -39,7 +39,7 @@ def hbnb():
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=uuid.uuid4())
+                           cache_id=str(uuid4()))
 
 
 if __name__ == "__main__":
